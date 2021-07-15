bd012c9eb571361d6d4268431289ddf0f2cf9bde
Joshua Dablo <john.dablo@nowcom.com.ph>
2021-07-15 17:46:27 +0800

    feat: initial search function

---
+++ src/constants/users.js
@@ -0,0 +1,20 @@
+export const allUsers = [
+    {
+        name: 'joshua',
+        age: 24,
+        description: 'I love mobile phones',
+        image: 'https://pickaface.net/gallery/avatar/unr_dummy_170202_1834_237aoxtc8.png'
+    },
+    {
+        name: 'angelo',
+        age: 17,
+        description: 'I love valorant',
+        image: 'https://pickaface.net/gallery/avatar/unr_dummy_170202_1834_237aoxtc8.png'
+    },
+    {
+        name: 'denielle',
+        age: 22,
+        description: 'I love baking',
+        image: 'https://pickaface.net/gallery/avatar/unr_dummy_170202_1834_237aoxtc8.png'
+    },
+]
