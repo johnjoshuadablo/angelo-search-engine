cb5f3b909dc9ef37e440912de37ff9a9416bce78
Joshua Dablo <john.dablo@nowcom.com.ph>
2021-07-19 17:33:34 +0800

    feat: added description ad search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { useState } from 'react';
-import {allUsers} from '../src/constants';
-import TextField from '@material-ui/core/TextField';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { useState } from "react";
+import { allUsers } from "../src/constants";
+import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 
-import './App.css';
+import "./App.css";
 
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      backgroundColor: 'pink',
-      width: '100%'
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      backgroundColor: "pink",
+      width: "100%",
     },
     textField: {
       margin: 20,
@@ -26,67 +26,71 @@ const useStyles = makeStyles(() =>
       borderWidth: 6,
     },
     listRoot: {
-      width: '100%',
+      width: "100%",
       // maxWidth: '36ch',
     },
     inline: {
-      display: 'inline',
+      display: "inline",
     },
-  }),
+  })
 );
 
 function App() {
   const classes = useStyles();
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   const onSearch = (text) => {
-    const newUsers = allUsers.filter(item => item.name.toLowerCase().includes(text.toLowerCase()))
-    setUsers(newUsers)
-  }
+    const newUsers = allUsers.filter(
+      (item) =>
+        item.name.toLowerCase().includes(text.toLowerCase()) ||
+        item.description.toLowerCase().includes(text.toLowerCase())
+    );
+    setUsers(newUsers);
+  };
 
   return (
-      <div className={classes.root}>
-        <h3>SEARCH A NAME</h3>
-        <TextField
-          onChange={(e) => onSearch(e.target.value)}
-          className={classes.textField}
-          placeholder="name"
-          id="standard-basic"
-          label="Search name"
-        />
-        <List className={classes.listRoot}>
-          {users.map((u) => {
-            return(
-              <div>
-                {/* <p>{u.name}</p>
+    <div className={classes.root}>
+      <h3>SEARCH A NAME</h3>
+      <TextField
+        onChange={(e) => onSearch(e.target.value)}
+        className={classes.textField}
+        placeholder="name"
+        id="standard-basic"
+        label="Search name"
+      />
+      <List className={classes.listRoot}>
+        {users.map((u) => {
+          return (
+            <div>
+              {/* <p>{u.name}</p>
                 <p>{u.age}</p>
                 <p>{u.description}</p> */}
-                <ListItem alignItems="flex-start">
-                  <ListItemAvatar>
-                    <Avatar alt={u.name} src={u.image} />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Brunch this weekend?"
-                    secondary={
-                      <>
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          className={classes.inline}
-                          color="textPrimary"
-                        >
-                          {u.name}
-                        </Typography>
-                        - {u.description}
-                      </>
-                    }
-                  />
-                </ListItem>
-              </div>
-            )
-          })}
-        </List>
-      </div>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt={u.name} src={u.image} />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        className={classes.inline}
+                        color="textPrimary"
+                      >
+                        {u.name}
+                      </Typography>
+                      - {u.description}
+                    </>
+                  }
+                />
+              </ListItem>
+            </div>
+          );
+        })}
+      </List>
+    </div>
   );
 }
 
